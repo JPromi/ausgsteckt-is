ca4d453c714ddddfb246f25c2ba50838c4946b19
JPromi <jonasmpr05@gmail.com>
2023-07-12 14:11:58 +0200

    add single view heurigen

--- src/app/components/heuriger/heuriger.component.ts
+++ src/app/components/heuriger/heuriger.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
+import { HeurigerService } from "../../services/heuriger.service";
 
 @Component({
   selector: 'app-heuriger',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./heuriger.component.scss']
 })
 export class HeurigerComponent {
+
+  heuriger:any = "";
+  mapsLink:string = "";
+
+  constructor(private service:HeurigerService, private route: ActivatedRoute, private router: Router) {
+    
+  }
+
+  ngOnInit() {
+    this.loadContent();
+  }
+
+  async loadContent(): Promise<Object> {
+    return this.route.params.subscribe(async heuriger => {
+      ((this.service.getHeuriger(heuriger["heuriger"])))
+      .subscribe((response: any) => {
+        this.heuriger = response;
+        this.generateMapsLink();
+        return heuriger["heuriger"];
+      })
+      ;
+    });
+  }
+
+  generateMapsLink() {
+    var url = "http://maps.google.com/maps?t=h&q=loc:";
+    this.mapsLink = url + this.heuriger.address.replace(" ", "+") + ',' +  '+' + this.heuriger.city.replace(" ", "+");
+  }
 
 }
