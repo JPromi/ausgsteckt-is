698dd5d611579332781b9a455dab4ad3f536c29e
JPromi <jonasmpr05@gmail.com>
2023-07-12 11:54:14 +0200

    add c

---
+++ src/app/components/heuriger/heuriger.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heuriger',
+  templateUrl: './heuriger.component.html',
+  styleUrls: ['./heuriger.component.scss']
+})
+export class HeurigerComponent {
+
+}
