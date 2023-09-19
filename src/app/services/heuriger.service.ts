3e8cdff2776274f6758b364090384be733e83e7b
JPromi <jonasmpr05@gmail.com>
2023-09-19 14:34:43 +0200

    add all heurigen services to one service file

--- src/app/services/heuriger.service.ts
+++ src/app/services/heuriger.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import cfg from '../../config.json';
+import { Heuriger } from '../dtos/heuriger';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeurigerService {
-  private url = cfg.api + cfg.apiVersion + '/heurigen';
+  private url = cfg.api + cfg.apiVersion;
    
   constructor(private httpClient: HttpClient) {}
   
-  getHeuriger(heurigerName:string){
-      return this.httpClient.get(this.url +  '?heurigen=' + heurigerName);
+  getHeuriger(heurigerName:string): Observable<Heuriger> {
+      return this.httpClient.get<Heuriger>(this.url +  '/heurigen?heurigen=' + heurigerName);
   }
+
+  getHeurigenByDate(date:string = ''): Observable<Heuriger> {
+    if(date) {
+      return this.httpClient.get<Heuriger>(this.url + '/ausgesteckt?date=' + date);
+    } else {
+      return this.httpClient.get<Heuriger>(this.url);
+    }
+  }
+
+  getAllHeurigen(): Observable<Heuriger> {
+    return this.httpClient.get<Heuriger>(this.url + '/heurigen');
+}
 }
