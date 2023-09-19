0b1b04672d2f94fbd040675b9fb7dd6a4e0b6fef
JPromi <jonasmpr05@gmail.com>
2023-09-19 14:34:29 +0200

    add dto

---
+++ src/app/dtos/heuriger.ts
@@ -0,0 +1,38 @@
+export class Heuriger {
+    constructor(
+        public id: number,
+        public name: string,
+        public nameId: string,
+        public address: string,
+        public city: string,
+        public coordinates: coordinates,
+        public playground: boolean,
+        public link: string,
+        public email: string,
+        public phone: phone,
+        public type: string,
+        public daysRemain: number,
+        public ausgsteckt: Array<ausgsteckt>
+    ) {}
+}
+
+export class coordinates {
+    constructor(
+        public lat: number,
+        public lng: number
+    ) {}
+}
+
+export class phone {
+    constructor(
+        public main: string,
+        public mobile: string
+    ) {}
+}
+
+export class ausgsteckt {
+    constructor(
+        public from: string,
+        public to: string
+    ) {}
+}
