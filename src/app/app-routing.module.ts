e643d07cbb639139d194f3bcc59d6f578d443af9
JPromi <jonasmpr05@gmail.com>
2023-09-19 13:19:41 +0200

    add maps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AllHeurigenComponent } from './components/all-heurigen/all-heurigen.component';
 import { CurrentHeurigenComponent } from './components/current-heurigen/current-heurigen.component';
 import { HeurigerComponent } from './components/heuriger/heuriger.component';
+import { MapsComponent } from './components/maps/maps.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/ausgsteckt', pathMatch: 'full' },
   { path: 'heurigen', component: AllHeurigenComponent },
   { path: 'heurigen/:heuriger', component: HeurigerComponent },
   { path: 'ausgsteckt', component: CurrentHeurigenComponent },
+  { path: 'karte', component: MapsComponent },
 ];
 
 @NgModule({
