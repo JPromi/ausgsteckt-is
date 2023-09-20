3a94d367b892033a4a5cf6bef6162c0a35b9b0ef
JPromi <jonasmpr05@gmail.com>
2023-09-20 08:55:14 +0200

    add header | date search | view all

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Heuriger, ausgsteckt, coordinates, phone } from 'src/app/dtos/heuriger';
 import { HeurigerService } from 'src/app/services/heuriger.service';
 
@@ -29,28 +31,83 @@ export class MapsComponent implements OnInit {
     zoom: 14,
   }
 
-  public openInfoWindow(marker: MapMarker, infoWindow: MapInfoWindow) {
-    infoWindow.open(marker);
-  }
-
   public heurigenList: Heuriger[] = [new Heuriger(0, '', '', '', '', new coordinates(0, 0), false, '', '', new phone('', '') , '', 0, [new ausgsteckt('', '')])];
   public heurigenLoaded = false;
 
-  constructor(
-    public heurigerService: HeurigerService,
-  ) {}
+  currentHeurigen:any;
+  parameter:any;
+  dateDisplay:any;
+  selectedDate = new FormControl('');
+  searchByDate = true;
 
-  ngOnInit(): void {
-    this.loadHeurigen();
+  constructor(
+    public heurigerService: HeurigerService, 
+    private route: ActivatedRoute, 
+    private router: Router
+  ) {
+    this.route.queryParams.subscribe(response => {
+      this.parameter = response;
+    }
+    );
   }
 
-  loadHeurigen() {
-    this.heurigerService.getHeurigenByDate().subscribe(
-      (response: Array<Heuriger>) => {
-        this.heurigenList = response;
-        this.heurigenLoaded = true
+  ngOnInit(dateP = ''): void {
+    var parameter = '';
+    if(dateP == '') {
+      if(!this.parameter.date) {
+        const currentDate = new Date();
+        parameter = currentDate.getFullYear() + '-' + this.checkDateZero(currentDate.getMonth() + 1) + '-' + this.checkDateZero(currentDate.getDate());
+      } else {
+        const currentDateT = Date.parse(this.parameter.date);
+        const currentDate = new Date(currentDateT);
+        parameter = currentDate.getFullYear() + '-' + this.checkDateZero(currentDate.getMonth() + 1) + '-' +this.checkDateZero(currentDate.getDate());
       }
-    );
+    } else {
+      parameter = dateP;
+    }   
+
+    this.loadHeurigen(parameter);
+
+    if(!this.parameter.date) {
+      const currentDate = new Date();
+      this.dateDisplay = currentDate.getDate() + '.' + (currentDate.getMonth() + 1) + '.' + currentDate.getFullYear();
+    } else {
+      const currentDateT = Date.parse(this.parameter.date);
+      const currentDate = new Date(currentDateT);
+      this.dateDisplay = currentDate.getDate() + '.' + (currentDate.getMonth() + 1) + '.' + currentDate.getFullYear();
+    }
+  }
+
+  loadHeurigen(parameter:any) {
+    if(this.searchByDate) {
+      this.heurigerService.getHeurigenByDate(parameter).subscribe(
+        (response: Array<Heuriger>) => {
+          this.heurigenList = response;
+          this.heurigenLoaded = true;
+  
+          if(!this.parameter.date) {
+            const currentDate = new Date();
+            this.dateDisplay = currentDate.getDate() + '.' + (currentDate.getMonth() + 1) + '.' + currentDate.getFullYear();
+          } else {
+            const currentDateT = Date.parse(this.parameter.date);
+            const currentDate = new Date(currentDateT);
+            this.dateDisplay = currentDate.getDate() + '.' + (currentDate.getMonth() + 1) + '.' + currentDate.getFullYear();
+          }
+        }
+      );
+    } else {
+      this.heurigerService.getAllHeurigen().subscribe(
+        (response: Array<Heuriger>) => {
+          this.heurigenList = response;
+          this.heurigenLoaded = true;
+        }
+      );
+    }
+    
+  }
+
+  openInfoWindow(marker: MapMarker, infoWindow: MapInfoWindow) {
+    infoWindow.open(marker);
   }
 
   generateMapsLink(heuriger: Heuriger) {
@@ -66,5 +123,73 @@ export class MapsComponent implements OnInit {
       }
     }
     return retrun; 
+  }
+
+  changeSearchType() {
+    if(this.searchByDate) {
+      this.searchByDate = false;
+    } else {
+      this.searchByDate = true;
+    }
+    this.loadHeurigen('');
+  }
+
+  // date
+  async lastDate() {
+    if(!this.parameter.date) {
+      var parameter = '';
+      const currentDate = new Date();
+      currentDate.setDate(currentDate.getDate() - 1);
+      parameter = await currentDate.getFullYear() + '-' + this.checkDateZero(currentDate.getMonth() + 1) + '-' + this.checkDateZero(currentDate.getDate());
+    } else {
+      const currentDateT = Date.parse(this.parameter.date);
+      const currentDate = new Date(currentDateT);
+      currentDate.setDate(currentDate.getDate() - 1);
+      parameter = await currentDate.getFullYear() + '-' + this.checkDateZero(currentDate.getMonth() + 1) + '-' +this.checkDateZero(currentDate.getDate());
+    }
+    
+    this.router.navigate(['/karte'], { queryParams: { date: parameter}});
+    
+    
+    this.loadHeurigen(parameter);
+  }
+
+  nextDate() {    
+    if(!this.parameter.date) {
+      var parameter = '';
+      const currentDate = new Date();
+      currentDate.setDate(currentDate.getDate() + 1);
+      parameter = currentDate.getFullYear() + '-' + this.checkDateZero(currentDate.getMonth() + 1) + '-' + this.checkDateZero(currentDate.getDate());
+    } else {
+      const currentDateT = Date.parse(this.parameter.date);
+      const currentDate = new Date(currentDateT);
+      currentDate.setDate(currentDate.getDate() + 1);
+      parameter = currentDate.getFullYear() + '-' + this.checkDateZero(currentDate.getMonth() + 1) + '-' +this.checkDateZero(currentDate.getDate());
+    }
+
+    this.router.navigate(['/karte'], { queryParams: { date: parameter}});
+    
+    
+    this.ngOnInit(parameter);
+  }
+
+  specificDate(event: any) {
+    const currentDateT = event.target.value;
+    const currentDate = new Date(currentDateT);
+    var parameter = currentDate.getFullYear() + '-' + this.checkDateZero(currentDate.getMonth() + 1) + '-' +this.checkDateZero(currentDate.getDate());
+
+    this.router.navigate(['/karte'], { queryParams: { date: parameter}});
+    
+    
+    this.ngOnInit(parameter);
+  }
+
+  checkDateZero(number:number):String {
+    let dateNumber = number.toString();
+    if(number < 10) {
+      dateNumber = '0' + number.toString();
+    }
+
+    return dateNumber;
   }
 }
