dbda00dc1114b0c90fdcd603a01b19667e1e66b8
JPromi <jonasmpr05@gmail.com>
2023-10-03 11:39:35 +0200

    add settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from './services/settings.service';
+import { Settings } from './dtos/settings';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ausgstekt-app';
+
+  settings: Settings = new Settings;
+
+  constructor(
+    private settingsSerive:SettingsService
+  ) {
+  }
+
+  async ngOnInit() {
+    await this.checkSetting();
+
+    // system darkmode
+    window.matchMedia("(prefers-color-scheme: dark)").addListener(e => {
+      this.settingsSerive.settings_darkmode();
+    });
+
+    // settings
+    addEventListener("storage", () => {
+      this.checkSetting();
+    });
+  }
+
+  checkSetting() {
+    if(!localStorage.length) {
+      this.settingsSerive.saveSettings(this.settings);
+    } else {
+      this.settings = this.settingsSerive.getSettings();
+    }
+
+    this.settingsSerive.settings_darkmode();
+  }
+
+  
 }
