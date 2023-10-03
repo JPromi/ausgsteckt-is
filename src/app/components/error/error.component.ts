1dcd3ebcf431492f37c471bbdb77e1b40a4b89b9
JPromi <jonasmpr05@gmail.com>
2023-10-03 13:15:34 +0200

    add darkmode to error page

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.service';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.scss']
 })
-export class ErrorComponent {
+export class ErrorComponent implements OnInit {
 
+  constructor (
+    public settingsService: SettingsService
+  ){}
+
+  ngOnInit(): void {
+    if(this.settingsService.getSettings().systemTheme) {
+      if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.getElementById('background')?.classList.add("night");
+      }
+    } else if(this.settingsService.getSettings().darkmode) {
+      document.getElementById('background')?.classList.add('night')
+    }
+  }
 }
