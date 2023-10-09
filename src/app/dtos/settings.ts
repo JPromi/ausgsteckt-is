856133e33ed89e13c2d7341df51f7a09fb4435f2
JPromi <jonasmpr05@gmail.com>
2023-10-09 09:17:49 +0200

    add languge settings

--- src/app/dtos/settings.ts
+++ src/app/dtos/settings.ts
@@ -2,5 +2,6 @@ export class Settings {
     constructor(
         public systemTheme:boolean = false,
         public darkmode:boolean = false,
+        public language:string = "de-AT"
     ) {}
 }
