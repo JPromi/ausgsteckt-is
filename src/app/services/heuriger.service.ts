42a991fe515f501ea779d50bdab52c94260cbd4f
JPromi <jonasmpr05@gmail.com>
2023-10-24 07:47:17 +0200

    change config json style; add timeout for http request

--- src/app/services/heuriger.service.ts
+++ src/app/services/heuriger.service.ts
@@ -3,28 +3,49 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import cfg from '../../config.json';
 import { Heuriger } from '../dtos/heuriger';
+import { timeout, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeurigerService {
-  private url = cfg.api + cfg.apiVersion;
+  private url = cfg.api.url + cfg.api.version;
    
   constructor(private httpClient: HttpClient) {}
   
   getHeuriger(heurigerName:string): Observable<Heuriger> {
-      return this.httpClient.get<Heuriger>(this.url +  '/heurigen?heurigen=' + heurigerName);
+      return this.httpClient.get<Heuriger>(this.url +  '/heurigen?heurigen=' + heurigerName).pipe(
+        timeout(cfg.api.timeout),
+        catchError(e => {
+          throw new Error('Timeout exceeded!');
+        })
+      );;
   }
 
   getHeurigenByDate(date:string = ''): Observable<Heuriger[]> {
     if(date) {
-      return this.httpClient.get<Heuriger[]>(this.url + '/ausgesteckt?date=' + date);
+      return this.httpClient.get<Heuriger[]>(this.url + '/ausgesteckt?date=' + date).pipe(
+        timeout(cfg.api.timeout),
+        catchError(e => {
+          throw new Error('Timeout exceeded!');
+        })
+      );;
     } else {
-      return this.httpClient.get<Heuriger[]>(this.url + '/ausgesteckt');
+      return this.httpClient.get<Heuriger[]>(this.url + '/ausgesteckt').pipe(
+        timeout(cfg.api.timeout),
+        catchError(e => {
+          throw new Error('Timeout exceeded!');
+        })
+      );;
     }
   }
 
   getAllHeurigen(): Observable<Heuriger[]> {
-    return this.httpClient.get<Heuriger[]>(this.url + '/heurigen');
-}
+    return this.httpClient.get<Heuriger[]>(this.url + '/heurigen').pipe(
+      timeout(cfg.api.timeout),
+      catchError(e => {
+        throw new Error('Timeout exceeded!');
+      })
+    );
+  }
 }
--- src/app/services/taxi.service.ts
+++ src/app/services/taxi.service.ts
@@ -3,16 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import cfg from '../../config.json';
 import { Taxi } from '../dtos/taxi';
+import { timeout, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaxiService {
-  private url = cfg.api + cfg.apiVersion;
+  private url = cfg.api.url + cfg.api.version;
 
   constructor(private httpClient: HttpClient) { }
 
   getTaxi(): Observable<Taxi[]> {
-    return this.httpClient.get<Taxi[]>(this.url +  '/taxi');
+    return this.httpClient.get<Taxi[]>(this.url +  '/taxi').pipe(
+      timeout(cfg.api.timeout),
+      catchError(e => {
+        throw new Error('Timeout exceeded!');
+      })
+    );;
 }
 }
